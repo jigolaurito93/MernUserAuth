46e6e79d99815113acea1415a3ac88b9179901cb
Jose Laurito <jigolaurito93@yahoo.com>
2024-03-17 01:22:23 -0500

    Create Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Login from "./views/Login";
 import Register from "./views/Register";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between bg-blue-100 py-4 px-36">
+      <div>Auth app</div>
+      <div className="flex gap-6 font-semibold">
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
